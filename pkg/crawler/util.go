67840886bfc10f32f1972730959833ce53060a05
HadeedTariq <computeranalog351@gmail.com>
2026-06-10 17:01:40 +0500

    integrate url cleaning and proper subdomain and domain checking functionality other then the crawling file supporting logic

---
+++ pkg/crawler/util.go
@@ -0,0 +1,242 @@
+package crawler
+
+import (
+	"encoding/base64"
+	"hash/fnv"
+	"io"
+	"log"
+	"mime"
+	"net/url"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/HadeedTariq/webora/pkg/links"
+	"github.com/s0rg/set"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+const (
+	proxyAuthKey   = "Proxy-Authorization"
+	proxyAuthBasic = "Basic"
+
+	contentType = "Content-Type"
+	contentHTML = "text/html"
+	contentCSS  = "text/css"
+	contentJS   = "application/javascript"
+	fileExtJS   = ".js"
+	fileExtCSS  = ".css"
+)
+
+var parsableExts = set.Load(make(set.Unordered[string]),
+	".asp",
+	".aspx",
+	".cgi",
+	".htm",
+	".html",
+	".jsp",
+	".php",
+	".pl",
+	".xhtml",
+	".xml",
+	fileExtJS,
+	fileExtCSS,
+)
+
+func proxyAuthHeader(v string) (rv string) {
+	return proxyAuthKey + ": " + proxyAuthBasic + " " + base64.StdEncoding.EncodeToString([]byte(v))
+}
+
+func prepareFilter(tags []string) links.TokenFilter {
+	if len(tags) == 0 {
+		return links.AllowALL
+	}
+
+	atoms := make(set.Unordered[atom.Atom])
+
+	var a atom.Atom
+
+	for _, t := range tags {
+		if a = atom.Lookup([]byte(t)); a != 0 {
+			atoms.Add(a)
+		} else {
+			log.Printf("[!] invalid tag: `%s` skipping...", t)
+		}
+	}
+
+	return func(t html.Token) (ok bool) {
+		return atoms.Has(t.DataAtom)
+	}
+}
+
+func canCrawl(a, b *url.URL, d int, subdomains bool) (yes bool) {
+	if a.Host != b.Host {
+		if !subdomains {
+			return false
+		}
+
+		domainA := strings.Split(a.Host, ".")
+		domainB := strings.Split(b.Host, ".")
+
+		if len(domainA) >= len(domainB) {
+			// The base domain must be shorter than the found domain
+			return false
+		}
+
+		j := len(domainB) - 1
+
+		for i := len(domainA) - 1; i >= 0 && j >= 0; i-- {
+			// Traverse each domain from the end, to check if their top-level domain are the same
+			if domainA[i] != domainB[j] {
+				// not the same top-level host
+				return false
+			}
+
+			j--
+		}
+	}
+
+	var apath, bpath string
+
+	if apath = a.Path; apath == "" {
+		apath = "/"
+	}
+
+	if bpath = b.Path; bpath == "" {
+		bpath = "/"
+	}
+
+	depth, found := relativeDepth(apath, bpath)
+	if !found {
+		return false
+	}
+
+	if d >= 0 && depth > d {
+		return false
+	}
+
+	return true
+}
+
+func relativeDepth(base, sub string) (n int, ok bool) {
+	var (
+		bn = path.Clean(base)
+		sn = path.Clean(sub)
+	)
+
+	if len(sn) < len(bn) {
+		return
+	}
+
+	if !strings.HasPrefix(sn, bn) {
+		return
+	}
+
+	const pathSep = '/'
+
+	fields := strings.FieldsFunc(sn[len(bn):], func(r rune) bool {
+		return r == pathSep
+	})
+
+	for i := 0; i < len(fields); i++ {
+		if fields[i] != "" {
+			n++
+		}
+	}
+
+	return n, true
+}
+
+func webExt(v string) (rv string) {
+	const maxCut = 2
+
+	p := strings.SplitN(v, "?", maxCut)
+
+	return filepath.Ext(p[0])
+}
+
+func canParse(v string) (yes bool) {
+	_, tmp := path.Split(v)
+	if tmp == "" {
+		return true
+	}
+
+	if tmp = webExt(tmp); tmp == "" {
+		return true
+	}
+
+	return parsableExts.Has(strings.ToLower(tmp))
+}
+
+func isSitemap(s string) (yes bool) {
+	const (
+		sitemapXML = "sitemap.xml"
+		sitemapIDX = "sitemap-index.xml"
+	)
+
+	return strings.HasSuffix(s, sitemapXML) || strings.HasSuffix(s, sitemapIDX)
+}
+
+func isResorce(v string) (yes bool) {
+	_, tmp := path.Split(v)
+	if tmp == "" {
+		return
+	}
+
+	if tmp = webExt(tmp); tmp == "" {
+		return
+	}
+
+	return true
+}
+
+func isHTML(v string) (yes bool) {
+	typ, _, err := mime.ParseMediaType(v)
+	if err != nil {
+		return
+	}
+
+	return typ == contentHTML
+}
+
+func isJS(v, n string) (yes bool) {
+	typ, _, err := mime.ParseMediaType(v)
+	if err == nil && typ == contentJS {
+		return true
+	}
+
+	return webExt(n) == fileExtJS
+}
+
+func isCSS(v, n string) (yes bool) {
+	typ, _, err := mime.ParseMediaType(v)
+	if err == nil && typ == contentCSS {
+		return true
+	}
+
+	return webExt(n) == fileExtCSS
+}
+
+func urlhash(s string) (rv uint64) {
+	hash := fnv.New64()
+	_, _ = io.WriteString(hash, strings.ToLower(s))
+
+	return hash.Sum64()
+}
+
+func resolveRef(base, uri string) (rv string, ok bool) {
+	b, err := url.Parse(base)
+	if err != nil {
+		return
+	}
+
+	u, err := url.Parse(uri)
+	if err != nil {
+		return
+	}
+
+	rv = b.ResolveReference(u).String()
+
+	return rv, true
+}
---
+++ pkg/crawler/util_test.go
@@ -0,0 +1,257 @@
+package crawler
+
+import (
+	"net/url"
+	"testing"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+func TestPrepareFilter(t *testing.T) {
+	t.Parallel()
+
+	f := prepareFilter([]string{"a", "form", "foo", "div"})
+	d := []html.Token{
+		{DataAtom: atom.A},
+		{DataAtom: atom.Form},
+		{DataAtom: atom.Div},
+	}
+
+	for _, k := range d {
+		if !f(k) {
+			t.Fatalf("not allowed: %v", k)
+		}
+	}
+
+	v := html.Token{DataAtom: atom.Video}
+
+	if f(v) {
+		t.Fatalf("allowed: %v", v)
+	}
+}
+
+func TestIsJS(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		Type string
+		Name string
+		Want bool
+	}
+
+	const (
+		nameHTML = "test.html"
+		nameJS   = "test.js"
+	)
+
+	cases := []testCase{
+		{Type: contentHTML, Name: nameHTML, Want: false},
+		{Type: contentJS, Name: nameJS, Want: true},
+		{Type: contentJS, Name: nameHTML, Want: true},
+		{Type: contentHTML, Name: nameJS, Want: true},
+		{Type: "", Name: nameHTML, Want: false},
+		{Type: "", Name: nameJS, Want: true},
+		{Type: "", Name: nameJS + "?v=1", Want: true},
+	}
+
+	for i, tc := range cases {
+		if isJS(tc.Type, tc.Name) != tc.Want {
+			t.Fatalf("case[%d] fail", i)
+		}
+	}
+}
+
+func TestRelativeDepth(t *testing.T) {
+	type args struct {
+		base string
+		sub  string
+	}
+
+	tests := []struct {
+		name      string
+		args      args
+		wantDepht int
+		wantFound bool
+	}{
+		{"a-ok", args{base: "/", sub: "/a"}, 1, true},
+		{"a-bad", args{base: "/a", sub: "/b"}, 0, false},
+		{"c-bad", args{base: "/a/b", sub: "/c"}, 0, false},
+		{"b-ok", args{base: "/a", sub: "/a/b"}, 1, true},
+		{"c-ok", args{base: "/a", sub: "/a/b/c"}, 2, true},
+		{"d-bad", args{base: "/a/b/c", sub: "/d/b/c/a"}, 0, false},
+	}
+
+	t.Parallel()
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			gotDepht, gotFound := relativeDepth(tc.args.base, tc.args.sub)
+			if gotDepht != tc.wantDepht {
+				t.Errorf("pathDepth() gotDepht = %v, want %v", gotDepht, tc.wantDepht)
+			}
+
+			if gotFound != tc.wantFound {
+				t.Errorf("pathDepth() gotFound = %v, want %v", gotFound, tc.wantFound)
+			}
+		})
+	}
+}
+
+func BenchmarkDepth(b *testing.B) {
+	const (
+		x = "/some/rather/long/path"
+		y = "/some/rather/long/path/but/longer"
+	)
+
+	for b.Loop() {
+		_, _ = relativeDepth(x, y)
+	}
+}
+
+func TestCanCrawl(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		b          *url.URL
+		u          *url.URL
+		d          int
+		subdomains bool
+	}
+
+	base, _ := url.Parse("http://test/some/path")
+	badh, _ := url.Parse("http://other/path")
+	url0, _ := url.Parse("http://test/some")
+	url1, _ := url.Parse("http://test/some/path/even")
+	url2, _ := url.Parse("http://test/some/path/even/more")
+	url3, _ := url.Parse("http://test")
+	url4, _ := url.Parse("http://abc.test/some")
+	url5, _ := url.Parse("http://abc.test/some/path")
+	url6, _ := url.Parse("http://abc.test/some/path/even")
+
+	tests := []struct {
+		name    string
+		args    args
+		wantYes bool
+	}{
+		{"url0-1", args{b: base, u: url0, d: 1, subdomains: false}, false},
+		{"url1-0", args{b: base, u: url1, d: 0, subdomains: false}, false},
+		{"url1-1", args{b: base, u: url1, d: 1, subdomains: false}, true},
+		{"url2-0", args{b: base, u: url2, d: 0, subdomains: false}, false},
+		{"url2-1", args{b: base, u: url2, d: 1, subdomains: false}, false},
+		{"url2-2", args{b: base, u: url2, d: 2, subdomains: false}, true},
+		{"url2-3", args{b: base, u: url2, d: 3, subdomains: false}, true},
+		{"badh-1", args{b: base, u: badh, d: 1, subdomains: false}, false},
+		{"url2-0-1", args{b: base, u: url0, d: -1, subdomains: false}, false},
+		{"url2-1-1", args{b: base, u: url1, d: -1, subdomains: false}, true},
+		{"url2-2-1", args{b: base, u: url2, d: -1, subdomains: false}, true},
+		{"url3-3", args{b: base, u: url3, d: 0, subdomains: false}, false},
+		{"url4-1", args{b: base, u: url4, d: 1000, subdomains: true}, false},
+		{"url5-1", args{b: base, u: url5, d: -1, subdomains: true}, true},
+		{"url5-2", args{b: base, u: url5, d: -1, subdomains: false}, false},
+		{"url6-1", args{b: base, u: url6, d: 1, subdomains: true}, true},
+		{"url6-2", args{b: base, u: url6, d: 0, subdomains: true}, false},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			if gotYes := canCrawl(tc.args.b, tc.args.u, tc.args.d, tc.args.subdomains); gotYes != tc.wantYes {
+				t.Errorf("canCrawl() = %v, want %v", gotYes, tc.wantYes)
+			}
+		})
+	}
+}
+
+func TestIsResorce(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		Val  string
+		Want bool
+	}
+
+	cases := []testCase{
+		{"/some/path", false},
+		{"/some/other/path/", false},
+		{"/path/to/some/resource.zip", true},
+	}
+
+	for _, tc := range cases {
+		if got := isResorce(tc.Val); got != tc.Want {
+			t.Errorf("failed for: '%s' got: %t", tc.Val, got)
+		}
+	}
+}
+
+func TestCanParse(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		Val  string
+		Want bool
+	}
+
+	cases := []testCase{
+		{"/some/path", true},
+		{"/some/other/path/", true},
+		{"/some/resource.html", true},
+		{"/path/to/some/resource.zip", false},
+	}
+
+	for _, tc := range cases {
+		if got := canParse(tc.Val); got != tc.Want {
+			t.Errorf("failed for: '%s' got: %t", tc.Val, got)
+		}
+	}
+}
+
+func TestIsSitemap(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		Val  string
+		Want bool
+	}
+
+	cases := []testCase{
+		{"/some/path", false},
+		{"/some/other/path/sitemap.xml", true},
+		{"/some/resource.html", false},
+		{"/path/to/some/sitemap-index.xml", true},
+	}
+
+	for _, tc := range cases {
+		if got := isSitemap(tc.Val); got != tc.Want {
+			t.Errorf("failed for: '%s' got: %t", tc.Val, got)
+		}
+	}
+}
+
+func TestUrlHash(t *testing.T) {
+	t.Parallel()
+
+	const val = "http://test/some/path?foo"
+
+	h1, h2 := urlhash(val), urlhash(val)
+
+	if h1 != h2 {
+		t.Error("hashes mismatch")
+	}
+}
+
+func TestProxyAuthHeader(t *testing.T) {
+	t.Parallel()
+
+	const (
+		got  = "user:pass"
+		want = "Proxy-Authorization: Basic dXNlcjpwYXNz"
+	)
+
+	if rv := proxyAuthHeader(got); rv != want {
+		t.Errorf("invalid header want: '%s' got: '%s'", want, rv)
+	}
+}
